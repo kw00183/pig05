ec4291287c1b5c52f6c219295aba079fb2a148a4
Kim Weible <kw00183@my.westga.edu>
2021-07-05 23:50:56 -0500

    PigPane - remove the NewGamePane class and create some public methods to control the enable/disable of the different game panes

--- src/edu/westga/cs6910/pig/view/PigPane.java
+++ src/edu/westga/cs6910/pig/view/PigPane.java
@@ -1,15 +1,7 @@
 package edu.westga.cs6910.pig.view;
 
 import edu.westga.cs6910.pig.model.Game;
-import edu.westga.cs6910.pig.model.Player;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -45,7 +37,7 @@ public class PigPane extends BorderPane {
 			throw new IllegalArgumentException("Invalid game");
 		}
 		this.theGame = theGame;
-		
+
 		this.helpDialog = new PigHelpDialog();
 		this.helpDialog.showHelpDialog();
 		this.pnContent = new BorderPane();
@@ -53,7 +45,7 @@ public class PigPane extends BorderPane {
 		this.menuBar = new PigMenuBar(this.theGame, this, this.helpDialog);
 		VBox menuBox = this.menuBar.createMenu();
 
-		this.pnChooseFirstPlayer = new NewGamePane(theGame);
+		this.pnChooseFirstPlayer = new NewGamePane(this.theGame, this);
 		HBox playerBox = this.createHBoxHolder(this.pnChooseFirstPlayer, false);
 
 		VBox topBox = new VBox();
@@ -104,158 +96,39 @@ public class PigPane extends BorderPane {
 	}
 
 	/**
-	 * Defines the panel in which the user selects which Player plays first.
+	 * Updates the status game pane
 	 */
-	private final class NewGamePane extends GridPane {
-		private RadioButton radHumanPlayer;
-		private RadioButton radComputerPlayer;
-		private RadioButton radRandomPlayer;
-		private ComboBox<Integer> cmbGoalScore;
+	public void updateGameInfo() {
+		PigPane.this.pnGameInfo.update();
+	}
 
-		private Game theGame;
-		private Player theHuman;
-		private Player theComputer;
+	/**
+	 * Enables or disables the player pane
+	 * 
+	 * @param isDisabled
+	 *            is the pane disabled
+	 */
+	public void showPlayerPane(boolean isDisabled) {
+		PigPane.this.pnChooseFirstPlayer.setDisable(isDisabled);
+	}
 
-		private NewGamePane(Game theGame) {
-			if (theGame == null) {
-				throw new IllegalArgumentException("Invalid game");
-			}
-			this.theGame = theGame;
+	/**
+	 * Enables or disables the computer pane
+	 * 
+	 * @param isDisabled
+	 *            is the pane disabled
+	 */
+	public void showComputerPane(boolean isDisabled) {
+		PigPane.this.pnComputerPlayer.setDisable(isDisabled);
+	}
 
-			this.theHuman = this.theGame.getHumanPlayer();
-			this.theComputer = this.theGame.getComputerPlayer();
-
-			this.buildPane();
-		}
-
-		private void buildPane() {
-			this.setHgap(20);
-
-			this.createGoalScoreItems();
-
-			this.createFirstPlayerItems();
-
-			this.reset();
-		}
-
-		private void createFirstPlayerItems() {
-			Label lblFirstPlayer = new Label("Who plays first? ");
-			this.add(lblFirstPlayer, 2, 0);
-
-			this.radHumanPlayer = new RadioButton(
-					this.theHuman.getName() + " first");
-			this.radHumanPlayer.setOnAction(new HumanFirstListener());
-
-			this.radComputerPlayer = new RadioButton(
-					this.theComputer.getName() + " first");
-			this.radComputerPlayer.setOnAction(new ComputerFirstListener());
-
-			this.radRandomPlayer = new RadioButton("Random Player");
-			this.radRandomPlayer.setOnAction(new RandomFirstListener());
-
-			ToggleGroup group = new ToggleGroup();
-			this.radHumanPlayer.setToggleGroup(group);
-			this.radComputerPlayer.setToggleGroup(group);
-			this.radRandomPlayer.setToggleGroup(group);
-
-			this.add(this.radHumanPlayer, 3, 0);
-			this.add(this.radComputerPlayer, 4, 0);
-			this.add(this.radRandomPlayer, 5, 0);
-		}
-
-		private void createGoalScoreItems() {
-			Label lblGoalScore = new Label("Initial Goal Score: ");
-			this.add(lblGoalScore, 0, 0);
-
-			this.cmbGoalScore = new ComboBox<Integer>();
-			this.cmbGoalScore.getItems().addAll(100, 50, 20);
-			this.cmbGoalScore.setValue(100);
-			this.cmbGoalScore.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent event) {
-					int goalScore = NewGamePane.this.cmbGoalScore.getValue();
-					PigPane.this.theGame.setGoalScore(goalScore);
-					PigPane.this.pnGameInfo.update();
-				}
-			});
-			this.add(this.cmbGoalScore, 1, 0);
-		}
-
-		/**
-		 * Resets the radio buttons for new selection
-		 */
-		public void reset() {
-			NewGamePane.this.theGame
-					.setGoalScore(NewGamePane.this.cmbGoalScore.getValue());
-			this.radHumanPlayer.setSelected(false);
-			this.radComputerPlayer.setSelected(false);
-			this.radRandomPlayer.setSelected(false);
-		}
-
-		/**
-		 * Defines the listener for computer player first button.
-		 */
-		private class RandomFirstListener implements EventHandler<ActionEvent> {
-			@Override
-			/**
-			 * Enables the ComputerPlayerPanel and starts a new game. Event
-			 * handler for a click in the computerPlayerButton.
-			 */
-			public void handle(ActionEvent arg0) {
-				int goalScore = NewGamePane.this.cmbGoalScore.getValue();
-
-				PigPane.this.pnChooseFirstPlayer.setDisable(true);
-
-				if (Math.random() * 10 <= 4) {
-					PigPane.this.pnComputerPlayer.setDisable(false);
-					PigPane.this.theGame.startNewGame(
-							NewGamePane.this.theComputer, goalScore);
-				} else {
-					PigPane.this.pnHumanPlayer.setDisable(false);
-					PigPane.this.theGame.startNewGame(NewGamePane.this.theHuman,
-							goalScore);
-				}
-			}
-		}
-
-		/*
-		 * Defines the listener for computer player first button.
-		 */
-		private class ComputerFirstListener
-				implements
-					EventHandler<ActionEvent> {
-			@Override
-			/**
-			 * Enables the ComputerPlayerPanel and starts a new game. Event
-			 * handler for a click in the computerPlayerButton.
-			 */
-			public void handle(ActionEvent arg0) {
-				int goalScore = NewGamePane.this.cmbGoalScore.getValue();
-
-				PigPane.this.pnComputerPlayer.setDisable(false);
-				PigPane.this.pnChooseFirstPlayer.setDisable(true);
-				PigPane.this.theGame.startNewGame(NewGamePane.this.theComputer,
-						goalScore);
-			}
-		}
-
-		/*
-		 * Defines the listener for human player first button.
-		 */
-		private class HumanFirstListener implements EventHandler<ActionEvent> {
-			/*
-			 * Sets up user interface and starts a new game. Event handler for a
-			 * click in the human player button.
-			 */
-			@Override
-			public void handle(ActionEvent event) {
-				int goalScore = NewGamePane.this.cmbGoalScore.getValue();
-
-				PigPane.this.pnChooseFirstPlayer.setDisable(true);
-				PigPane.this.pnHumanPlayer.setDisable(false);
-				PigPane.this.theGame.startNewGame(NewGamePane.this.theHuman,
-						goalScore);
-			}
-		}
+	/**
+	 * Enables or disables the human pane
+	 * 
+	 * @param isDisabled
+	 *            is the pane disabled
+	 */
+	public void showHumanPane(boolean isDisabled) {
+		PigPane.this.pnHumanPlayer.setDisable(isDisabled);
 	}
 }
